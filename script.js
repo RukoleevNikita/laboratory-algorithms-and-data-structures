9902b9ee3874dcb91dc3e38648a70bdd6bd44602
RukoleevNikita <53287449+RukoleevNikita@users.noreply.github.com>
2020-09-27 18:42:57 +0300

    Генерация строки и вставка в таблицу

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
-// table = '<table border="1"><tr><th>Имя</th><th>Фамилия</th><th>Вид спорта</th><th>Результат</th></tr>'
-document.getElementById('addButton').onclick = function() {
-    let arrValue = Array.from(document.getElementsByTagName('input'), el => el.value)
-    // document.getElementsByTagName('input').value = ' ' //!не работает отчистка полей
-    console.log(arrValue)
-    for(let i = 0; i < arrValue.length; i++) {
-        tableLine = '<tr><td>' + arrValue[i] + '</td></tr></table>' 
-    }
-    console.log(tableLine)
-    document.getElementById('table').append(tableLine)
-}
+ $(document).ready(function(){
+     var arrValue = []
+    $('#addButton').on('click', function() {
+        // let arrValue = Array.from(document.getElementsByTagName('input'), el => el.value)
+        // console.log(arrValue)
 
-// var d = document
-// function addRow() {
-//     // Считываем значения с формы
-//     surnname = d.getElementById('surnname').value
-//     name = d.getElementById('name').value
-//     sport = d.getElementById('sport').value
-//     result = d.getElementById('result').value
+        let surnname = $('#surname').val()
+        let name     = $('#name').val()
+        let sport    = $('#sport').val()
+        let result   = $('#result').val()
 
-//     // Находим нужную таблицу
-//     var tbody = d.getElementById('tab1').getElementsByTagName('TBODY')[0]
+        let arrTemp = []
 
-//     // Создаем строку таблицы и добавляем ее
-//     var row = d.createElement("TR")
-//     tbody.appendChild(row)
+        arrTemp['surname'] = surnname
+        arrTemp['name'] = name
+        arrTemp['sport'] = sport
+        arrTemp['result'] = result
 
-//     // Создаем ячейки в вышесозданной строке
-//     // и добавляем тх
-//     var td1 = d.createElement("TD")
-//     var td2 = d.createElement("TD")
-//     var td3 = d.createElement("TD")
-//     var td4 = d.createElement("TD")
+        arrValue.push(arrTemp)
+        console.log(arrValue)
 
-//     row.appendChild(td1)
-//     row.appendChild(td2)
-//     row.appendChild(td3)
-//     row.appendChild(td4)
-
-//     // Наполняем ячейки
-//     td1.innerHTML = surnname
-//     td2.innerHTML = name
-//     td3.innerHTML = sport
-//     td4.innerHTML = result
+        let tableLine = '<tr><td>' + surnname + '</td><td>' + name + '</td><td>' + sport + '</td><td>' + result + '</td></tr>'
+        // document.getElementsByTagName('input').value = ' ' //!не работает отчистка полей
+        let table = $('#table')
+        table.append(tableLine)
+    })
+})
+// window.onload = function(){
+//     function multi() {
+//         // Считываем значения с формы
+//         surnname = document.getElementById('surnname').value
+//         name = document.getElementById('name').value
+//         sport = document.getElementById('sport').value
+//         result = document.getElementById('result').value
+    
+//         // Находим нужную таблицу
+//         var tbody = document.getElementById('tab1').getElementsByTagName('TBODY')[0]
+    
+//         // Создаем строку таблицы и добавляем ее
+//         var row = document.createElement("TR")
+//         tbody.appendChild(row)
+    
+//         // Создаем ячейки в вышесозданной строке
+//         // и добавляем тх
+//         var td1 = document.createElement("TD")
+//         var td2 = document.createElement("TD")
+//         var td3 = document.createElement("TD")
+//         var td4 = document.createElement("TD")
+    
+//         row.appendChild(td1)
+//         row.appendChild(td2)
+//         row.appendChild(td3)
+//         row.appendChild(td4)
+    
+//         // Наполняем ячейки
+//         td1.innerHTML = surnname
+//         td2.innerHTML = name
+//         td3.innerHTML = sport
+//         td4.innerHTML = result
+//     }
+   
 // }
+
 
 
