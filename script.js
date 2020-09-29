f8151721e95a97eff2d23a846dc5991dc8ddec3c
RukoleevNikita <53287449+RukoleevNikita@users.noreply.github.com>
2020-09-29 23:32:59 +0300

    Вёрстка добавление в таблицу

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
  $(document).ready(function(){
      var arrValue = []
-     var arrSortSport = []
     $('#addButton').on('click', function() {
 
         let surnname = $('#surname').val()
@@ -18,31 +17,35 @@
         arrValue[arrValue.length] = arrTemp
 
         let tableLine = '<tr><td>' + surnname + '</td><td>' + name + '</td><td>' + sport + '</td><td>' + result + '</td></tr>'
-        // document.getElementsByTagName('input').value = ' ' //!не работает отчистка полей
         var table = $('#table')
         table.append(tableLine)
+        $('input').val('')
     })
     $('#sortSport').on('click', function() {
-        // document.querySelector('#resultSortTab').innerHTML = `<table class="tablSortSport"><thead><tr><th>Фамилия</th><th>Имя</th><th>Вид спорта</th><th>Результат</th></tr></thead></table>`
         for(var i = 1; i < arrValue.length; i++){
             for(var j = 0; j < arrValue.length-i; j++){
                 if(arrValue[j]['sport'].substring(0, 1) > arrValue[j + 1]['sport'].substring(0, 1)){
                     var temp = arrValue[j]
                     arrValue[j] = arrValue[j + 1]
                     arrValue[j + 1] = temp
-                    arrSortSport = arrValue
                 }
-            }  
-        } 
-        for(var k = 0; k < arrSortSport.length; k++) {
-            for(var t = 0; t < arrSortSport[k].length; t++) {
-                var lineTable = '<tr><td>' + arrSortSport[k]['surname'] + '</td><td>' + arrSortSport[k]['name'] + '</td><td>' + arrSortSport[k]['sport'] + '</td><td>' + arrSortSport[k]['result'] + '</td></tr>'
-                var tab = $('#tableSort')
-                tab.append(lineTable)
-            }console.log(arrSortSport[k])
-            
+            } 
+        }  
+        // if($('#tableSort tr').html() !== '') {
+        //     $('#tableSort tr').html() = ''
+        // }
+        for(var t = 0; t < arrValue.length; t++) {
+            var lineTable = '<tr><td>' + arrValue[t]['surname'] + '</td><td>' + arrValue[t]['name'] + '</td><td>' + arrValue[t]['sport'] + '</td><td>' + arrValue[t]['result'] + '</td></tr>'
+            var tab = $('#tableSort')
+            tab.append(lineTable)
         }
-    }) 
+
+    })
+    
+    // отключить событие клика
+    // $('#sortSport').mousedown(function() { 
+	//     $('#sortSport').css('pointer-events', 'none' ); 
+	//   });
     
 })
 
