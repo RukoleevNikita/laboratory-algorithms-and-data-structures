785a13c39655f2305c71460ad185665683a6aa58
RukoleevNikita <53287449+RukoleevNikita@users.noreply.github.com>
2020-10-13 20:01:33 +0300

    Валидация форм

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
- $(document).ready(function(){
-     var arrValue = []
-    $('#addButton').on('click', function() {
+$(document).ready(function () {
+    var arrValue = []
+    $('#addButton').on('click', function () {
 
         let surnname = $('#surname').val()
-        let name     = $('#name').val()
-        let sport    = $('#sport').val()
-        let result   = $('#result').val()
+        let name = $('#name').val()
+        let sport = $('#sport').val()
+        let result = $('#result').val()
 
         let arrTemp = []
 
         arrTemp['surname'] = surnname
         arrTemp['name'] = name
         arrTemp['sport'] = sport
-        arrTemp['result'] = result
+        arrTemp['result'] = parseInt(result)
+
 
         arrValue[arrValue.length] = arrTemp
 
         let tableLine = '<tr><td>' + surnname + '</td><td>' + name + '</td><td>' + sport + '</td><td>' + result + '</td></tr>'
-        var table = $('#table')
+        let table = $('#table')
         table.append(tableLine)
         $('input').val('')
     })
-    $('#sortSport').on('click', function() {
-        for(var i = 1; i < arrValue.length; i++){
-            for(var j = 0; j < arrValue.length-i; j++){
-                if(arrValue[j]['sport'].substring(0, 1) > arrValue[j + 1]['sport'].substring(0, 1)){
+
+    $('#sortSport').on('click', function () {
+        for (var i = 1; i < arrValue.length; i++) {
+            for (var j = 0; j < arrValue.length - i; j++) {
+                if (arrValue[j]['sport'].substring(0, 1) > arrValue[j + 1]['sport'].substring(0, 1)) {
                     var temp = arrValue[j]
                     arrValue[j] = arrValue[j + 1]
                     arrValue[j + 1] = temp
                 }
-            } 
-        }  
-        // if($('#tableSort tr').html() !== '') {
-        //     $('#tableSort tr').html() = ''
-        // }
-        for(var t = 0; t < arrValue.length; t++) {
-            var lineTable = '<tr><td>' + arrValue[t]['surname'] + '</td><td>' + arrValue[t]['name'] + '</td><td>' + arrValue[t]['sport'] + '</td><td>' + arrValue[t]['result'] + '</td></tr>'
-            var tab = $('#tableSort')
-            tab.append(lineTable)
+            }
         }
 
+        if ($('#tableSort').find('tr') !== '') {
+            $('#tableSort').find('>tr').remove()
+        }
+
+        for (var t = 0; t < arrValue.length; t++) {
+            let lineTable = '<tr><td>' + arrValue[t]['surname'] + '</td><td>' + arrValue[t]['name'] + '</td><td>' + arrValue[t]['sport'] + '</td><td>' + arrValue[t]['result'] + '</td></tr>'
+            let table = $('#tableSort')
+            table.append(lineTable)
+        }
     })
-    
-    // отключить событие клика
-    // $('#sortSport').mousedown(function() { 
-	//     $('#sortSport').css('pointer-events', 'none' ); 
-	//   });
-    
+
+    $('#failsResult').on('click', function () {
+        for (var iFail = 1; iFail < arrValue.length; iFail++) {
+            for (var jFail = 0; jFail < arrValue.length - iFail; jFail++) {
+                if (arrValue[jFail]['result'] < arrValue[jFail + 1]['result']) {
+                    var tempFail = arrValue[jFail]
+                    arrValue[jFail] = arrValue[jFail + 1]
+                    arrValue[jFail + 1] = tempFail
+                }
+            }
+        }
+
+        if ($('#failsTable').find('tr') !== '') {
+            $('#failsTable').find('>tr').remove()
+        }
+
+        for (var x = 0; x < arrValue.length; x++) {
+            let lineTableFails = '<tr><td>' + arrValue[x]['surname'] + '</td><td>' + arrValue[x]['result'] + '</td></tr>'
+            let table = $('#failsTable')
+            table.append(lineTableFails)
+        }
+    })
 })
 
